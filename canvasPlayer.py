ea3a1a4dc3c806b5d04cfeb875f93eb2cb0c71c7
kms70847 <kevin.m.smiley@gmail.com>
2014-05-20 22:23:14 -0400

    use space for indentation

--- canvasPlayer.py
+++ canvasPlayer.py
@@ -3,16 +3,16 @@ import threading
 import time
 
 class CanvasPlayer:
-	def __init__(self, state, player, canvas):
-		self.state = state
-		self.player = player
-		self.canvas = canvas
-		self.canvas.bind("<Button-1>", self.clicked)
-	def clicked(self, e):
-		if self.state.whoseTurn() != self.player or self.state.isGameOver():
-			return
-		x = e.x * self.state.width / int(self.canvas.cget("width"))
-		y = e.y * self.state.height/ int(self.canvas.cget("height"))
-		
-		if self.state.canPlace(x,y):
-			self.state.place(x,y)
+    def __init__(self, state, player, canvas):
+        self.state = state
+        self.player = player
+        self.canvas = canvas
+        self.canvas.bind("<Button-1>", self.clicked)
+    def clicked(self, e):
+        if self.state.whoseTurn() != self.player or self.state.isGameOver():
+            return
+        x = e.x * self.state.width / int(self.canvas.cget("width"))
+        y = e.y * self.state.height/ int(self.canvas.cget("height"))
+        
+        if self.state.canPlace(x,y):
+            self.state.place(x,y)
